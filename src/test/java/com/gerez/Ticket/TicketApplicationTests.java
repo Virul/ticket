c5066b087d27989fb1f9d1a48c19e8f330989216
Raul Gerez <rgerez@ecitonglobal.com>
2024-08-05 14:46:54 -0600

    Configuracion travis

--- src/test/java/com/gerez/Ticket/TicketApplicationTests.java
+++ src/test/java/com/gerez/Ticket/TicketApplicationTests.java
@@ -1,13 +1,26 @@
 package com.gerez.Ticket;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(TicketApplicationTests.class)
+@RunWith(SpringRunner.class)
 class TicketApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	public void testWelcome() throws Exception {
+		mockMvc.perform(get("/hello")).andExpect(status().isOk());
 	}
 
 }
